813c1dac934177a0773de9214f319ad5f566dedb
hoangdoan8222-hash <hoangdoan8222@gmail.com>
2025-10-16 21:42:06 +0700

    Add files via upload

---
+++ BTVN2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	int height = 30;
+	int width = 30;
+	int sum = height + width;
+	int minus = height - width;
+	int multi = height * width;
+	int div = height / width;
+	int dev2 = height % width;
+}

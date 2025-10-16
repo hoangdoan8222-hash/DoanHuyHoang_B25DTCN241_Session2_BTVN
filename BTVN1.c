aafde406385654ae40116d8ac0cae57c151bb919
hoangdoan8222-hash <hoangdoan8222@gmail.com>
2025-10-16 21:41:46 +0700

    Add files via upload

---
+++ BTVN1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+	
+int main(){
+	int number1= 803;  
+	float point = 555.77;
+	char letter = 'a';
+	printf("gia tri %d \n",number1);
+	printf("gia tri 2 %.1f \n" ,point);
+	printf("gia tri %d",number1);
+}

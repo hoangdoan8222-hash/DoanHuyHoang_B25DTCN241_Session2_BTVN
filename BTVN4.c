bd4dd8e5e58826c71e7fb5a212545f94e34ccdd6
hoangdoan8222-hash <hoangdoan8222@gmail.com>
2025-10-16 21:42:58 +0700

    Add files via upload

---
+++ BTVN4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	 int chieuDai = 10;
+     int chieuRong = 5;
+      // Khai báo bi?n luu k?t qu?
+    int chuVi, dienTich;
+
+    // Tính chu vi và di?n tích
+    chuVi = 2 * (chieuDai + chieuRong);
+    dienTich = chieuDai * chieuRong;
+
+    // Hi?n th? k?t qu?
+    printf("Chieu dai: %d\n", chieuDai);
+    printf("Chieu rong: %d\n", chieuRong);
+    printf("Chu vi hinh chu nhat: %d\n", chuVi);
+    printf("Dien tich hinh chu nhat: %d\n", dienTich);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+    
